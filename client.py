fa6d5851cc8652bba8295830296fc7cafc3e501a
Abdullah <45091790+AMK98@users.noreply.github.com>
2018-12-25 20:47:07 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,8 +4,7 @@ serverport=12000
 BUFFER_SIZE=1024
 client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 while 1:
-    c = input('pleas input the price range you have :')  # cpu values are cor 2, i3 , i5 , i7 ,i9-> input writ Just int EX ( 2 ,3 ,5 ,7 ,9)
-    message=c
+    c = input('pleas input the price range you have :')  # inputing the price range of the user
     client.sendto(message.encode('utf-8'),(serverip,serverport))
     response,address=client.recvfrom(BUFFER_SIZE)
     print (message +'$'+''+'the computer stats you can affourd are : '+response.decode('utf-8'))
